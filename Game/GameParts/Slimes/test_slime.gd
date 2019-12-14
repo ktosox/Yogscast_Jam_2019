80d97ef2792df8478f1d391c73c16cbc08b5cc06
ktosox <ktoso3@gmail.com>
2019-12-15 00:16:34 +0100

    adding some slime renders

--- Game/GameParts/Slimes/test_slime.gd
+++ Game/GameParts/Slimes/test_slime.gd
@@ -5,6 +5,17 @@ var target_pos
 var is_moving = false
 
 func _ready():
+	match(randi()%5):
+		0:
+			$Sprite.texture = load("res://GameParts/Slimes/slime_spritesheet_blue.png")
+		1:
+			$Sprite.texture = load("res://GameParts/Slimes/slime_spritesheet_grn.png")
+		2:
+			$Sprite.texture = load("res://GameParts/Slimes/slime_spritesheet_red.png")
+		3:
+			$Sprite.texture = load("res://GameParts/Slimes/slime_spritesheet_orng.png")
+		4:
+			$Sprite.texture = load("res://GameParts/Slimes/slime_spritesheet_prpl.png")
 	doSomething()
 
 func use_gem(in_gem):
@@ -22,6 +33,9 @@ func doSomething():
 	applied_force = Vector2((randf()-0.5)*300,(randf()-0.5)*300)
 	pass
 
+func _physics_process(delta):
+	if (abs(rotation_degrees)>5):
+		angular_velocity = delta * (-rotation) * 60
 
 func _on_TimerBored_timeout():
 	doSomething()
